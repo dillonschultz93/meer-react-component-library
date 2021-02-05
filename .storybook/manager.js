c0cedabdfa89f05ddaaa2219bd5959614925afbc
Dillon Schultz <dillonschultz93@gmail.com>
2021-02-04 20:29:57 -0700

    Theme storybook

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import meerTheme from './meer-theme';
+
+addons.setConfig({
+  theme: meerTheme,
+});
---
+++ .storybook/meer-theme.js
@@ -0,0 +1,17 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Meer React Component Library',
+  colorPrimary: '#5044ff',
+  colorSecondary: '#5044ff',
+  appBg: '#f9f9f9',
+  appContentBg: '#f9f9f9',
+  appBorderRadius: 4,
+  fontBase: "BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+  fontCode: 'monospace',
+  textColor: '#393f47',
+  textInverseColor: '#f9f9f9',
+  barTextColor: '#393f47',
+  barSelectedColor: '#5044ff'
+});
